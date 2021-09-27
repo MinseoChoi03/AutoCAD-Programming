97215d02573ca41153130d9f121682b02fa02f3e
Minseo Choi <78577815+MinseoChoi03@users.noreply.github.com>
2021-09-27 16:19:20 +0900

    ListLayers

--- AutoCAD_11/Class1.cs
+++ AutoCAD_11/Class1.cs
@@ -1,12 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.Geometry;
 
 namespace AutoCAD_11
 {
     public class Class1
     {
+        [CommandMethod("ListLayers")]
+        public static void ListLayers()
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            Database db = doc.Database;
+
+            using (Transaction trans = db.TransactionManager.StartTransaction())
+            {
+                LayerTable lyTab = trans.GetObject(db.LayerTableId, OpenMode.ForRead) as LayerTable;
+                foreach (ObjectId lyID in lyTab)
+                {
+                    LayerTableRecord lytr = trans.GetObject(lyID, OpenMode.ForRead) as LayerTableRecord;
+                    doc.Editor.WriteMessage("\nLayer name: " + lytr.Name);
+                }
+
+                // Commit the transaction
+                trans.Commit();
+            }
+        }
     }
 }
