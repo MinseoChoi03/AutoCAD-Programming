05e7a48ab471f13ebeabfcdaccc960b26a7a46c3
Minseo Choi <78577815+MinseoChoi03@users.noreply.github.com>
2021-09-27 15:26:56 +0900

    초기 커밋.

---
+++ AutoCAD_10/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.Geometry;
+
+namespace AutoCAD_10
+{
+    public class Class1
+    {
+    }
+}
