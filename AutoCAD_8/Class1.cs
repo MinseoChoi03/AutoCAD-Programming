2f1014360cac8cbaf13b45aadc1c944ac2cac0fa
Minseo Choi <78577815+MinseoChoi03@users.noreply.github.com>
2021-09-27 13:30:58 +0900

    modify

--- AutoCAD_8/Class1.cs
+++ AutoCAD_8/Class1.cs
@@ -9,6 +9,44 @@ namespace AutoCAD_8
 {
     public class Class1
     {
+        [CommandMethod("DrawMText")]
+        public void DrawMText()
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            Database db = doc.Database;
+            Editor edt = doc.Editor;
+
+            using (Transaction trans = db.TransactionManager.StartTransaction())
+            {
+                try
+                {
+                    edt.WriteMessage("Drawing MText Exercise!");
+                    BlockTable bt;
+                    bt = trans.GetObject(db.BlockTableId, OpenMode.ForRead) as BlockTable;
+
+                    BlockTableRecord btr;
+                    btr = trans.GetObject(bt[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
+
+                    //Specify the MTexts parameters (e.g. Textrstring, insertionPoin)
+                    string txt = "Hello AutoCAD from C#!";
+                    Point3d insPt = new Point3d(200, 200, 0);
+                    using (MText mtx = new MText())
+                    {
+                        mtx.Contents = txt;
+                        mtx.Location = insPt;
+
+                        btr.AppendEntity(mtx);
+                        trans.AddNewlyCreatedDBObject(mtx, true);
+                    }
+                    trans.Commit();
+                }
+                catch (System.Exception ex)
+                {
+                    edt.WriteMessage("Error encountered : " + ex.Message);
+                    trans.Abort();
+                }
+            }
+        }
         [CommandMethod("DraqLine")]
         public void DraqLine()
         {
@@ -29,7 +67,7 @@ namespace AutoCAD_8
                     //Send a message to the user
                     edt.WriteMessage("\n Drawing a Line object : ");
                     Point3d pt1 = new Point3d(0, 0, 0);
-                    Point3d pt2 = new Point3d(100, 00, 0);
+                    Point3d pt2 = new Point3d(100, 100, 0);
                     Line In = new Line(pt1, pt2);
 
                     In.ColorIndex = 1; //Color is red
