b0fca03e6d1b07997c357dad0318e2a59b7b95a6
Minseo Choi <78577815+MinseoChoi03@users.noreply.github.com>
2021-09-27 10:27:00 +0900

    Encapsulation

---
+++ AutoCAD_6/Person.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoCAD_6
+{
+    class Person
+    {
+        // Declare the Fields
+        private string _firstName = "DefaultFirstName";
+        private string _lastName = "DefaultLastName";
+        private int _age = 20;
+
+        // Declare the Properties
+        public string FirstName
+        {
+            get { return _firstName; }
+            set
+            {
+                if (value.Length <= 20 && value != null)
+                {
+                    _firstName = value;
+                }
+            }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+
+        // Constructors
+        public Person() { }
+
+        public Person(string firstName, string lastName, int age)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+            _age = age;
+        }
+
+        // Methods
+        public string DoWork()
+        {
+            // do some other work here
+            DoWorkPrivately();
+            return "I do general labour work";
+        }
+        private void DoWorkPrivately()
+        {
+            Console.WriteLine("I do gardening.");
+            Console.WriteLine("I clean the house.");
+            Console.WriteLine("I walk the dog.");
+        }
+
+        public void DisplayPersonInfo()
+        {
+            Console.WriteLine("Your Fullname is: {0} {1} and your age is {2}", _firstName, _lastName, _age);
+        }
+    }
+}
--- AutoCAD_6/Program.cs
+++ AutoCAD_6/Program.cs
@@ -10,6 +10,16 @@ namespace AutoCAD_6
     {
         static void Main(string[] args)
         {
+            //Example 1 - using the fields default values
+            //--------------------------------------------------
+            Person person1 = new Person();
+            person1.DisplayPersonInfo();
+
+            //Example 2 - using the fields
+            // --------------------------------------------------
+            Person person2 = new Person("Joy", "Frazer", 32);
+            person2.DisplayPersonInfo();
+            Console.WriteLine("My Type of work is: " + person2.DoWork());
         }
     }
 }
