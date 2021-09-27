668520e190d7fe1e2508750293ed323b88eea8f6
Minseo Choi <78577815+MinseoChoi03@users.noreply.github.com>
2021-09-27 16:20:12 +0900

    CreateLayer

--- AutoCAD_11/Class1.cs
+++ AutoCAD_11/Class1.cs
@@ -2,6 +2,7 @@
 using Autodesk.AutoCAD.ApplicationServices;
 using Autodesk.AutoCAD.DatabaseServices;
 using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.Colors;
 using Autodesk.AutoCAD.Geometry;
 
 namespace AutoCAD_11
@@ -25,6 +26,37 @@ namespace AutoCAD_11
 
                 // Commit the transaction
                 trans.Commit();
+            }
+        }
+        [CommandMethod("CreateLayer")]
+        public static void CreateLayer()
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            Database db = doc.Database;
+
+            using (Transaction trans = db.TransactionManager.StartTransaction())
+            {
+                LayerTable lyTab = trans.GetObject(db.LayerTableId, OpenMode.ForRead) as LayerTable;
+                if (lyTab.Has("Misc"))
+                {
+                    doc.Editor.WriteMessage("Layer already exist.");
+                    trans.Abort();
+                }
+                else
+                {
+                    lyTab.UpgradeOpen();
+                    LayerTableRecord ltr = new LayerTableRecord();
+                    ltr.Name = "Misc";
+                    ltr.Color = Color.FromColorIndex(ColorMethod.ByLayer, 1);
+                    lyTab.Add(ltr);
+                    trans.AddNewlyCreatedDBObject(ltr, true);
+                    db.Clayer = lyTab["Misc"];
+
+                    doc.Editor.WriteMessage("Layer [" + ltr.Name + "] was created successfully.");
+
+                    // Commit the transaction
+                    trans.Commit();
+                }
             }
         }
     }
