3c036ade46a7da8ace962cbf7d581d294b644988
Minseo Choi <78577815+MinseoChoi03@users.noreply.github.com>
2021-09-27 11:37:14 +0900

    초기 커밋.

---
+++ AutoCAD_8/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.EditorInput;
+
+namespace AutoCAD_8
+{
+    public class Class1
+    {
+       
+    }
+}
