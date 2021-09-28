3df03ae52f66dd5755f27984705019015e677463
Minseo Choi <78577815+MinseoChoi03@users.noreply.github.com>
2021-09-28 14:37:57 +0900

    A4TitleBlock

--- AutoCAD_14/A4TitleBlock.cs
+++ AutoCAD_14/A4TitleBlock.cs
@@ -6,8 +6,15 @@ using Autodesk.AutoCAD.Geometry;
 
 namespace AutoCAD_14
 {
-    class A4TitleBlock
+    class A4TitleBlock : TBlock
     {
+        public int Height { get; set; }
+        public int Width { get; set; }
 
+        public A4TitleBlock()
+        {
+            Height = 210;
+            Width = 297;
+        }
     }
 }
