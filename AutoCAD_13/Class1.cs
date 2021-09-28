3594eb11f928edc9e556a8270119bf13328340e0
Minseo Choi <78577815+MinseoChoi03@users.noreply.github.com>
2021-09-28 11:39:43 +0900

    SelectAllAndChangeLayer

--- AutoCAD_13/Class1.cs
+++ AutoCAD_13/Class1.cs
@@ -1,12 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
 
 namespace AutoCAD_13
 {
     public class Class1
     {
+        [CommandMethod("SelectAllAndChangeLayer")]
+        public void SelectAllAndChangeLayer()
+        {
+            // Get the document, database, and editor object
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            Database db = doc.Database;
+            Editor edt = doc.Editor;
+
+            using (Transaction trans = doc.TransactionManager.StartTransaction())
+            {
+                // Create a selectionset and select all the object in the drawing
+                PromptSelectionResult ssPrompt = edt.SelectAll();
+                SelectionSet ss = ssPrompt.Value;
+
+                // Check if there is object selected
+                if (ssPrompt.Status == PromptStatus.OK)
+                {
+                    // Loop through the selectionset and change the layer
+                    foreach (SelectedObject sObj in ss)
+                    {
+                        if (sObj != null)
+                        {
+                            // Create a new entity and assign the object to that entity
+                            Entity ent = trans.GetObject(sObj.ObjectId, OpenMode.ForWrite) as Entity;
+                            ent.Layer = "Misc";
+                        }
+                    }
+                    // Commit the transaction
+                    trans.Commit();
+                }
+                else
+                {
+                    edt.WriteMessage("No object selected.");
+                }
+            }
+        }
     }
 }
