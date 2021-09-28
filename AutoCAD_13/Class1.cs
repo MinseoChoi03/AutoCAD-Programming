f50d7391865aafd7b6527fd0daf99007709d5509
Minseo Choi <78577815+MinseoChoi03@users.noreply.github.com>
2021-09-28 13:07:06 +0900

    SelectObjectOnScreen

--- AutoCAD_13/Class1.cs
+++ AutoCAD_13/Class1.cs
@@ -45,5 +45,38 @@ namespace AutoCAD_13
                 }
             }
         }
+
+        [CommandMethod("SelectObjectOnScreen")]
+        public void SelectObjectOnScreen()
+        {
+            // Get the current drawing document and database
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            Database db = doc.Database;
+            Editor edt = doc.Editor;
+
+            using (Transaction trans = db.TransactionManager.StartTransaction())
+            {
+                // Prompt the user to select objects
+                PromptSelectionResult psr = edt.GetSelection();
+                if (psr.Status == PromptStatus.OK)
+                {
+                    // Create a selectionset and assign the selected objects
+                    SelectionSet ss = psr.Value;
+                    edt.WriteMessage("\nThere are a total of " + ss.Count.ToString() + " objects selected.");
+
+                    foreach (SelectedObject sObj in ss)
+                    {
+                        // Open the selected object for write and assign to an Entity object
+                        Entity ent = trans.GetObject(sObj.ObjectId, OpenMode.ForWrite) as Entity;
+                        if (ent != null)
+                        {
+                            ent.ColorIndex = 1;
+                        }
+                    }
+                }
+                // Commit the transaction
+                trans.Commit();
+            }
+        }
     }
 }
