3775eb18e4220139e086f641908f342f4ed8c30a
Minseo Choi <78577815+MinseoChoi03@users.noreply.github.com>
2021-09-28 14:41:31 +0900

    A1TitleBlock

---
+++ AutoCAD_14/A1TitleBlock.cs
@@ -0,0 +1,14 @@
+﻿namespace AutoCAD_14
+{
+    class A1TitleBlock
+    {
+        public int Height { get; set; }
+        public int Width { get; set; }
+
+        public A1TitleBlock()
+        {
+            Height = 593;
+            Width = 841;
+        }
+    }
+}
