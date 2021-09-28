8df2f14074b37b6cadb709b8a712b62583147055
Minseo Choi <78577815+MinseoChoi03@users.noreply.github.com>
2021-09-28 14:40:03 +0900

    A3TitleBlock

--- AutoCAD_14/A3TitleBlock.cs
+++ AutoCAD_14/A3TitleBlock.cs
@@ -2,6 +2,13 @@
 {
     class A3TitleBlock
     {
+        public int Height { get; set; }
+        public int Width { get; set; }
 
+        public A3TitleBlock()
+        {
+            Height = 297;
+            Width = 420;
+        }
     }
 }
