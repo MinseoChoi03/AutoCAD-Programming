7199904fbc9d5a1d6162438ef97fa39978b9ebfe
Minseo Choi <78577815+MinseoChoi03@users.noreply.github.com>
2021-09-28 14:40:54 +0900

    A2TitleBlock

---
+++ AutoCAD_14/A2TitleBlock.cs
@@ -0,0 +1,14 @@
+﻿namespace AutoCAD_14
+{
+    class A2TitleBlock
+    {
+        public int Height { get; set; }
+        public int Width { get; set; }
+
+        public A2TitleBlock()
+        {
+            Height = 420;
+            Width = 594;
+        }
+    }
+}
